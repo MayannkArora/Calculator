b526542ed546f56f2d7c2680d196d50c140bd9f0
nouvelleapprenant <101288088+nouvelleapprenant@users.noreply.github.com>
2022-08-31 00:23:30 +0530

    Add .js file with query Selectors

---
+++ app.js
@@ -0,0 +1,46 @@
+// Defining the elements in screen 
+
+let num1 = document.querySelector('#num1');
+let num2 = document.querySelector('#num2');
+let operator = document.querySelector('#operator');
+let sign = document.querySelector('#equalSign');
+let result = document.querySelector('#result');
+let slash = document.querySelector('#slash');
+
+
+// Defining the elments in keys
+
+// Defining elements in row 1
+
+let one = document.querySelector('#one');
+let two = document.querySelector('#two');
+let three = document.querySelector('#three');
+let add = document.querySelector('#add');
+
+// Defining elements in row 2
+
+let four = document.querySelector('#four');
+let five = document.querySelector('#five');
+let six = document.querySelector('#six');
+let subtract = document.querySelector('#subtract');
+
+// Defining elements in row 3
+
+let seven = document.querySelector('#seven');
+let eight = document.querySelector('#eight');
+let nine = document.querySelector('#nine');
+let multiply = document.querySelector('#multiply');
+
+// Defining elements in row 4
+
+let clear = document.querySelector('#clear');
+let zero = document.querySelector('#zero');
+let equal = document.querySelector('#equalTo');
+let divide = document.querySelector('#divide');
+
+// Defining elements in row 5
+
+let star = document.querySelector('#star');
+let dot = document.querySelector('#dot');
+let hash = document.querySelector('#hash');
+let sqrt = document.querySelector('#sqrt');
